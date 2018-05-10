d11d47e9838940faf7a5d6a2be2dfbf8124f4f0b
Luis Freitas <luisffreitasp@gmail.com>
2018-05-10 13:35:10 +0100

    Included file for Main module implementation (CGI-part) of the project.

---
+++ Main.hs
@@ -0,0 +1,4 @@
+module Main where
+
+-- TODO: module that'll interact with a web service (CGI-style) in order to show a visual representation of the interpreter
+-- featuring evaluation mode selection, showing the reduction graph and intermediate evaluation steps.
